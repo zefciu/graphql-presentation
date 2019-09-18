96907c238cca8bca0fa02c81c4b18ee3f175dd80
Szymon Pyżalski <szymon@pythonista.net>
2019-09-18 08:41:25 +0200

    Backend dependencies

--- project/backend/setup.py
+++ project/backend/setup.py
@@ -5,4 +5,14 @@ setup(
     version='0.1',
     description='Example GraphQL application',
     packages=['movie_base'],
+    install_requires=[
+        'alembic==1.1.0',
+        'Flask==1.1.1',
+        'Flask-Cors==3.0.8',
+        'Flask-GraphQL==2.0.0',
+        'graphene==2.1.8',
+        'graphene-sqlalchemy==2.2.2',
+        'PyYAML==5.1.2',
+        'SQLAlchemy==1.3.8',
+    ]
 )
